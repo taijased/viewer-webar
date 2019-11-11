7675dee6b14de7d9eae09c6dec387d53b8dba286
Spiridonov Maxim <taijased@gmail.com>
2019-11-11 15:53:05 +0300

    Create test

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// ARFRAME.registerComponent('registerevents', {
+//     init: function() {
+//         var marker = this.el;
+//         marker.setAttribute('emmitevents', 'true')
+//         marker.addEventListener('markerFound', function() {
+//             var markerId = marker.id
+//             console.log('markerFound', markerId);
+//         });
+//         marker.addEventListener('markerLost', function() {
+//             var markerId = marker.id
+//             console.log('markerLost', markerId);
+//         });
+//     }
+// });
+
+console.log("loh");
